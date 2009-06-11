898bff2078d52cac5ace6c461c4abad8e94d9523
brian.quinlan <brian.quinlan@localhost>
2009-06-11 20:03:23 +0000

    Added setup scripts.

---
+++ python2/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='futures',
+      version='0.1',
+      description='Java-style futures implementation in Python',
+      author='Brian Quinlan',
+      author_email='brian@sweetapp.com',
+      url='http://code.google.com/p/pythonfutures',
+      download_url='http://pypi.python.org/pypi/futures/',
+      packages=['futures'],
+      license='BSD',
+      classifiers=['License :: OSI Approved :: BSD License',
+                   'Development Status :: 3 - Alpha',
+                   'Intended Audience :: Developers',
+                   'Programming Language :: Python']
+      )
