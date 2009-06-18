3d95f8e2a312037ceb374a92ceec135a764d04a7
brian.quinlan <brian.quinlan@localhost>
2009-06-18 07:33:51 +0000

    Increment version number to 0.2

--- python2/setup.py
+++ python2/setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='futures',
-      version='0.1',
+      version='0.2',
       description='Java-style futures implementation in Python',
       author='Brian Quinlan',
       author_email='brian@sweetapp.com',
