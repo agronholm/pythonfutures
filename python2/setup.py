a62bca6299df56e6bc7fbd48d387d7281943de59
brian.quinlan <brian.quinlan@localhost>
2009-10-25 09:02:47 +0000

    Update the version number and use different targets for python 2.x and 3.x

--- python2/setup.py
+++ python2/setup.py
@@ -3,16 +3,16 @@
 from distutils.core import setup
 
 setup(name='futures',
-      version='0.2',
+      version='1.0',
       description='Java-style futures implementation in Python',
       author='Brian Quinlan',
       author_email='brian@sweetapp.com',
       url='http://code.google.com/p/pythonfutures',
-      download_url='http://pypi.python.org/pypi/futures/',
+      download_url='http://pypi.python.org/pypi/futures3/',
       packages=['futures'],
       license='BSD',
       classifiers=['License :: OSI Approved :: BSD License',
-                   'Development Status :: 3 - Alpha',
+                   'Development Status :: 5 - Production/Stable',
                    'Intended Audience :: Developers',
                    'Programming Language :: Python :: 2']
       )
--- python3/setup.py
+++ python3/setup.py
@@ -2,17 +2,17 @@
 
 from distutils.core import setup
 
-setup(name='futures',
-      version='0.2',
-      description='Java-style futures implementation in Python',
+setup(name='futures3',
+      version='1.0',
+      description='Java-style futures implementation in Python 3.x',
       author='Brian Quinlan',
       author_email='brian@sweetapp.com',
       url='http://code.google.com/p/pythonfutures',
-      download_url='http://pypi.python.org/pypi/futures/',
+      download_url='http://pypi.python.org/pypi/futures3/',
       packages=['futures'],
       license='BSD',
       classifiers=['License :: OSI Approved :: BSD License',
-                   'Development Status :: 3 - Alpha',
+                   'Development Status :: 5 - Production/Stable',
                    'Intended Audience :: Developers',
                    'Programming Language :: Python :: 3']
       )
