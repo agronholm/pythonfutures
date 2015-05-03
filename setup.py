bf5eaa018ab2b2cbc75374a2cb676eef337d7cc9
Alex Grönholm <alex.gronholm@nextday.fi>
2015-05-03 08:36:27 +0300

    Bumped up version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except ImportError:
     from distutils.core import setup
 
 setup(name='futures',
-      version='2.2.0',
+      version='3.0.0',
       description='Backport of the concurrent.futures package from Python 3.2',
       author='Brian Quinlan',
       author_email='brian@sweetapp.com',
